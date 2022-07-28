e393c518c220ed417e853f9b254697a9de3d910d
Nicolas Papaianni <papaianninicolas@gmail.com>
2022-07-29 00:05:48 +0200

    limpieza de proyecto

--- primermvt/familiares/views.py
+++ primermvt/familiares/views.py
@@ -3,14 +3,12 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from familiares.models import Familiares
 
-# Create your views here.
-
 def create_persona(request):
-    nueva_persona = Familiares.objects.create(nombre = 'Paula Gonzalez', fecha_nacimiento = '1960-8-10' , edad = 62)
+    create_persona = Familiares.objects.create(nombre = 'Paula Gonzalez', fecha_nacimiento = '1960-8-10' , edad = 62)
     context = {
-        'nueva_persona' : nueva_persona
+        'create_persona' : create_persona
     }
-    return render(request, 'nueva_persona.html', context = context)
+    return render(request, 'create_persona.html', context = context)
 
 
 def lista_familiares(request):
