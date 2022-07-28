470d9276fa917e3c54d96867a5d9aa58dd08d006
Nicolas Papaianni <papaianninicolas@gmail.com>
2022-07-29 00:18:14 +0200

    pasos ejecutados al crear personas

--- primermvt/familiares/views.py
+++ primermvt/familiares/views.py
@@ -4,7 +4,9 @@ from django.shortcuts import render
 from familiares.models import Familiares
 
 def create_persona(request):
-    create_persona = Familiares.objects.create(nombre = 'Paula Gonzalez', fecha_nacimiento = '1960-8-10' , edad = 62)
+    # create_persona = Familiares.objects.create(nombre = 'Nicolas Papaianni', fecha_nacimiento = '1988-07-10' , edad = 34)
+    # create_persona = Familiares.objects.create(nombre = 'Martin Papaianni', fecha_nacimiento = '1990-09-10' , edad = 32)
+    create_persona = Familiares.objects.create(nombre = 'Paula Gonzalez', fecha_nacimiento = '1960-8-10' , edad = 62) 
     context = {
         'create_persona' : create_persona
     }
